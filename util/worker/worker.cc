d0cd02636e4420f020d623d1eb2de618bcdb6008
Nikhil Marathe <nsm.nikhil@gmail.com>
2021-03-23 22:13:45 -0700

    Now capable of parsing the WorkRequest

--- util/worker/worker.cc
+++ util/worker/worker.cc
@@ -18,10 +18,42 @@
 #include <string>
 #include <utility>
 
+#include <google/protobuf/io/coded_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+
 #include "util/process_wrapper/system.h"
 #include "util/process_wrapper/utils.h"
+#include "util/worker/worker_protocol.pb.h"
+
+// TODO: Library and app split.
+using blaze::worker::WorkRequest;
+using google::protobuf::io::CodedInputStream;
+using google::protobuf::io::FileInputStream;
 
 using CharType = process_wrapper::System::StrType::value_type;
+
+bool ReadRequest(CodedInputStream *stream, WorkRequest *request)
+{
+  uint32_t req_len;
+  if (!stream->ReadVarint32(&req_len)) {
+    std::cerr << "Unable to read message length\n";
+    return false;
+  }
+
+  CodedInputStream::Limit limit = stream->PushLimit(req_len);
+  if (!request->MergeFromCodedStream(stream)) {
+    std::cerr << "Unable to merge from stream\n";
+    return false;
+  }
+
+  if (!stream->ConsumedEntireMessage()) {
+    std::cerr << "Did not consume entire message\n";
+    return false;
+  }
+
+  stream->PopLimit(limit);
+  return true;
+}
 
 // Simple process wrapper allowing us to not depend on the shell to run a
 // process to perform basic operations like capturing the output or having
@@ -38,6 +70,9 @@ int PW_MAIN(int argc, const CharType* argv[], const CharType* envp[]) {
 
   using Subst = std::pair<System::StrType, System::StrType>;
 
+  // This will need support for understanding param file argument.
+  // As well as parsing other flags generally.
+
   System::StrType exec_path;
   std::vector<Subst> subst_mappings;
   System::StrType stdout_file;
@@ -48,116 +83,38 @@ int PW_MAIN(int argc, const CharType* argv[], const CharType* envp[]) {
   System::Arguments arguments;
   System::Arguments file_arguments;
 
+  bool as_worker = false;
+
   // Processing current process argument list until -- is encountered
   // everthing after gets sent down to the child process
   for (int i = 1; i < argc; ++i) {
     System::StrType arg = argv[i];
-    if (++i == argc) {
-      std::cerr << "process wrapper error: argument \"" << ToUtf8(arg)
-                << "\" missing parameter.\n";
-      return -1;
-    }
-    if (arg == PW_SYS_STR("--subst")) {
-      System::StrType subst = argv[i];
-      size_t equal_pos = subst.find('=');
-      if (equal_pos == std::string::npos) {
-        std::cerr << "process wrapper error: wrong substituion format for \""
-                  << ToUtf8(subst) << "\".\n";
-        return -1;
-      }
-      System::StrType key = subst.substr(0, equal_pos);
-      if (key.empty()) {
-        std::cerr << "process wrapper error: empty key for substituion \""
-                  << ToUtf8(subst) << "\".\n";
-        return -1;
-      }
-      System::StrType value = subst.substr(equal_pos + 1, subst.size());
-      if (value == PW_SYS_STR("${pwd}")) {
-        value = System::GetWorkingDirectory();
-      }
-      subst_mappings.push_back({std::move(key), std::move(value)});
-    } else if (arg == PW_SYS_STR("--env-file")) {
-      if (!ReadFileToArray(argv[i], environment_block)) {
-        return -1;
-      }
-    } else if (arg == PW_SYS_STR("--arg-file")) {
-      if (!ReadFileToArray(argv[i], file_arguments)) {
-        return -1;
-      }
-    } else if (arg == PW_SYS_STR("--touch-file")) {
-      if (!touch_file.empty()) {
-        std::cerr << "process wrapper error: \"--touch-file\" can only appear "
-                     "once.\n";
-        return -1;
-      }
-      touch_file = argv[i];
-    } else if (arg == PW_SYS_STR("--copy-output")) {
-      // i is already at the first arg position, accountint we need another arg
-      // and then -- executable_name.
-      if (i + 1 > argc) {
-        std::cerr
-            << "process wrapper error: \"--copy-output\" needs 2 parameters.\n";
-        return -1;
-      }
-      if (!copy_source.empty() || !copy_dest.empty()) {
-        std::cerr << "process wrapper error: \"--copy-output\" can only appear "
-                     "once.\n";
-        return -1;
-      }
-      copy_source = argv[i];
-      copy_dest = argv[++i];
-      if (copy_source == copy_dest) {
-        std::cerr << "process wrapper error: \"--copy-output\" source and dest "
-                     "need to be different.\n";
-        return -1;
-      }
-    } else if (arg == PW_SYS_STR("--stdout-file")) {
-      if (!stdout_file.empty()) {
-        std::cerr << "process wrapper error: \"--stdout-file\" can only appear "
-                     "once.\n";
-        return -1;
-      }
-      stdout_file = argv[i];
-    } else if (arg == PW_SYS_STR("--stderr-file")) {
-      if (!stderr_file.empty()) {
-        std::cerr << "process wrapper error: \"--stderr-file\" can only appear "
-                     "once.\n";
-        return -1;
-      }
-      stderr_file = argv[i];
-    } else if (arg == PW_SYS_STR("--")) {
-      exec_path = argv[i];
-      for (++i; i < argc; ++i) {
-        arguments.push_back(argv[i]);
-      }
-      // after we consume all arguments we append the files arguments
-      for (const System::StrType& file_arg : file_arguments) {
-        arguments.push_back(file_arg);
-      }
+    if (arg == PW_SYS_STR("--persistent_worker")) {
+        as_worker = true;
     } else {
-      std::cerr << "process wrapper error: unknow argument \"" << ToUtf8(arg)
+      std::cerr << "worker wrapper error: unknown argument \"" << ToUtf8(arg)
                 << "\"." << '\n';
       return -1;
     }
   }
 
-  if (subst_mappings.size()) {
-    for (const Subst& subst : subst_mappings) {
-      System::StrType token = PW_SYS_STR("${");
-      token += subst.first;
-      token.push_back('}');
-      for (System::StrType& arg : arguments) {
-        ReplaceToken(arg, token, subst.second);
-      }
-
-      for (System::StrType& env : environment_block) {
-        ReplaceToken(env, token, subst.second);
-      }
-    }
+  if (!as_worker) {
+    std::cerr << "Don't know how to run as non-worker yet\n";
+    return -1;
   }
 
-  std::cerr << "NIKHILM HELLO\n";
-  return 1;
+  CodedInputStream *stream = new CodedInputStream(new FileInputStream(0));
+  while (true) {
+    WorkRequest request;
+    if (!ReadRequest(stream, &request)) {
+      return 1;
+    }
+    for (int i = 0; i < request.arguments_size(); i++) {
+      std::cerr << request.arguments(i) << " ";
+    }
+    std::cerr << "\n";
+    return 1;
+  }
 
   // Have the last values added take precedence over the first.
   // This is simpler than needing to track duplicates and explicitly override them.
